0e20d82ab049a29ec38cd094b2d07e3a9f3f06e1
murratore <mail@michael-maurer.ch>
2022-07-04 15:44:15 +0000

    AuthenticationService implemente (with Dependency Injection)

---
+++ BuberDinner.Application/Services/Authentication/IAuthenticationService.cs
@@ -0,0 +1,9 @@
+namespace BuberDinner.Application.Services.Authentication;
+
+public interface IAuthenticationService
+{
+    AuthenticationResult Register(string firstName, string LastName, string email, string password);
+
+    AuthenticationResult Login(string email, string password);
+    
+}
